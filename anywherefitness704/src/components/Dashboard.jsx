860792cec3a5632d7a523ebe1416e5dac17ada60
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-07-27 14:05:30 -0600

    wip

--- anywherefitness704/src/components/Dashboard.jsx
+++ anywherefitness704/src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom'
 import styled from "styled-components"
 import { backgroundColor } from '../Styles'
 import { Card, Button } from '@material-ui/core';
 
-export default function Dashboard() {
 
+export default function Dashboard() {
+    const history = useHistory()
 
     return (
         <>
@@ -13,17 +15,17 @@ export default function Dashboard() {
             <StyledCard elevation={3}>
                 <h3>Courses</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quidem dolor aliquid illo ab recusandae, voluptates ipsam necessitatibus sapiente officia magni expedita, ea eius voluptate explicabo magnam quaerat, quis doloremque.</p>
-                <StyledButton variant="outlined" color="primary">See courses</StyledButton>
+                <StyledButton variant="outlined" color="primary" onClick={() => history.push('/courses')}>See courses</StyledButton>
             </StyledCard>
             <StyledCard elevation={3}>
                 <h3>Instructors</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quidem dolor aliquid illo ab recusandae, voluptates ipsam necessitatibus sapiente officia magni expedita, ea eius voluptate explicabo magnam quaerat, quis doloremque.</p>
-                <StyledButton variant="outlined" color="primary">See instructors</StyledButton>
+                <StyledButton variant="outlined" color="primary" onClick={() => history.push('/instructors')}>See instructors</StyledButton>
             </StyledCard>
             <StyledCard elevation={3}>
                 <h3>Locations</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quidem dolor aliquid illo ab recusandae, voluptates ipsam necessitatibus sapiente officia magni expedita, ea eius voluptate explicabo magnam quaerat, quis doloremque.</p>
-                <StyledButton variant="outlined" color="primary">See locations</StyledButton>
+                <StyledButton variant="outlined" color="primary" onClick={() => history.push('/locations')}>See locations</StyledButton>
             </StyledCard>
         </StyledSection>
         </>
