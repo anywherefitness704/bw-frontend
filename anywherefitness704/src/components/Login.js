db2b2123089abc4e051cdeab63a56a645b69d11e
Chayce SOlchaga <chayce.solchaga@gmail.com>
2020-07-27 13:16:42 -0700

    created inputs on login form

--- anywherefitness704/src/components/Login.js
+++ anywherefitness704/src/components/Login.js
@@ -1,9 +1,50 @@
 import React from 'react';
 
+//using React Hook Form for validation
+import { useForm } from 'react-hook-form'
+
 export default function Login() {
+    // properties the hook returns
+    const { register, handleSubmit, errors } = useForm();
+
+    // used to handle the form submission and get the filled data.
+    function onSubmit(data) {
+        console.log("Data submitted: ", data);
+      }
 
 
     return (
-        <div> Login.js </div>
+        <div className="login-form">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="inputEmail">E-mail:&nbsp;&nbsp;
+          <input
+            type="email"
+            id="inputEmail"
+            name="email"
+            ref={register({
+                required: "Enter your e-mail",
+                pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Enter a valid e-mail address",
+                },
+            })}
+          />
+          {errors.email && <p className="error">{errors.email.message}</p>}
+          </label>
+            <br />
+          <label htmlFor="inputPassword">Password:&nbsp;&nbsp;
+          <input
+            type="password"
+            id="inputPassword"
+            name="password"
+            ref={register({
+                required: "You must specify a password" })}
+          />
+          {errors.password && <p className="error">{errors.password.message}</p>}
+          </label>
+            <br />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     )
 }
