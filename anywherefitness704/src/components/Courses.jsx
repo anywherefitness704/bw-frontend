aff428c19160062dd13d7bc9307234bcbf234aea
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-07-27 15:10:05 -0600

    wip

--- anywherefitness704/src/components/Courses.jsx
+++ anywherefitness704/src/components/Courses.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { courseListData } from '../resources/dummyData'
+
+import styled from "styled-components"
+import { 
+  Card,
+  Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Expand
+} from '@material-ui/core';
+import { ExpandMore } from '@material-ui/icons'
+
+const initialCourseList = []
 
 export default function Courses() {
+  const [courseList, setCourseList] = useState(initialCourseList)
+
+  useEffect(() => { // throw network stuff here later
+    setCourseList(courseListData)
+  }, [])
 
   return (
     <>
       <h2>Courses</h2>
-      
+      <StyledSection>
+        {courseList && courseList.map(course => (
+          <StyledAccordion elevation={3}>
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+            <p>{course.name}</p>
+          </AccordionSummary>
+
+            <StyledButton>See course page</StyledButton>
+          </StyledAccordion>
+        ))}
+      </StyledSection>
     </>
   )
 }
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`
+const StyledAccordion = styled(Accordion)`
+  width: 60%;
+`
+const StyledButton = styled(Button)`
+    &:hover {
+        box-shadow: 1px 1px 3px black;
+    }
+`
--- anywherefitness704/src/resources/dummyData.js
+++ anywherefitness704/src/resources/dummyData.js
@@ -4,41 +4,47 @@ export const courseListData = [
     location: '5345 4015 W, Taylorsville, UT 84129',
     description: 'Come learn to breath with the reast of the mouth breathers!',
     cost: '',
-    instructor: ''
+    instructor: '',
+    id: 1
   },
   {
     name: 'Yoga and Meditions',
     location: '60, Hero Honda Rd, Udyog Vihar Industrial Area Phase VI, Sector 37, Gurugram, Haryana 122001, India',
     description: 'Contort yourself, who needs a pretzel? We\'ll show you how to impress your friends with your own ligaments.',
     cost: '',
-    instructor: ''
+    instructor: '',
+    id: 2
   },
   {
     name: 'Native Leg Workouts',
     location: 'Zuni, NM 87327',
     description: 'Come on down to the reservation! We know how to work every muscle in your legs.',
     cost: '',
-    instructor: ''
+    instructor: '',
+    id: 3
   },
   {
     name: 'Cooking Keto Meals While Suspended Upside Down',
     location: '5223 NE Sandy Blvd, Portland, OR 97213',
     description: 'A diet and fitness class with an inverted twist.',
     cost: '',
-    instructor: ''
+    instructor: '',
+    id: 4
   },
   {
     name: 'Tribal Dance',
     location: '332 Crescent St, Quincy, CA 95971',
     description: 'Come and make your family proud! Learn to dance like never before.',
     cost: '',
-    instructor: ''
+    instructor: '',
+    id: 5
   },
   {
     name: 'Zuzi-Zumba',
     location: '820 Lake Baldwin Ln, Orlando, FL 32803',
     description: 'Florida man learns Zumba. So can you!',
     cost: '',
-    instructor: ''
+    instructor: '',
+    id: 6
   }
 ]
