9e61eb928e6fa4a2d3a2d393211bfb1aeb0a56d8
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-07-28 12:20:52 -0600

    simplified Course component by removing state and just using context

--- anywherefitness704/src/components/Instructors.jsx
+++ anywherefitness704/src/components/Instructors.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+import { CoursesContext } from "../contexts/CoursesContext";
+
+import styled from "styled-components";
 
 export default function Instructors() {
-
   return (
     <>
       <h2>Instructors</h2>
     </>
-  )
+  );
 }
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
